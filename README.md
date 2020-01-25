06419a0ff222538d854d73db577a5f42b1b61e43
Kael Zhang <i+github@kael.me>
2020-01-25 15:04:22 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,10 @@
+[![Build Status](https://travis-ci.org/kaelzhang/python-binance-sdk.svg?branch=master)](https://travis-ci.org/kaelzhang/python-binance-sdk)
+[![Coverage](https://codecov.io/gh/kaelzhang/python-binance-sdk/branch/master/graph/badge.svg)](https://codecov.io/gh/kaelzhang/python-binance-sdk)
+
+# binance-sdk
+
+Unofficial Binance python SDK
+
+## License
+
+[MIT](LICENSE)
---
+++ setup.py
@@ -0,0 +1,35 @@
+import os
+from setuptools import setup
+from binance import __version__
+
+# Utility function to read the README file.
+# Used for the long_description.  It's nice, because now 1) we have a top level
+# README file and 2) it's easier to type in the README file than to put a raw
+# string in below ...
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+setup(
+    name = 'binance-sdk',
+    packages = ['binance'],
+    version = __version__,
+    author = 'Kael Zhang',
+    author_email = 'i@kael.me',
+    description = ('Binance Python SDK'),
+    license = 'MIT',
+    keywords = 'binance exchange sdk rest api bitcoin btc bnb ethereum eth neo',
+    url = 'https://github.com/kaelzhang/python-neuronjs',
+    long_description=read('README.rst'),
+    classifiers=[
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: Implementation :: PyPy',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'License :: OSI Approved :: MIT License',
+    ]
+)
