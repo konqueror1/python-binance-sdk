f5e80e4f548608117b127fbe24d72ed7b278a85e
Kael Zhang <i+github@kael.me>
2020-02-17 20:15:56 +0800

    test_api: more tests

--- test/test_api.py
+++ test/test_api.py
@@ -1,7 +1,8 @@
 import pytest
 import requests_mock
 
-from binance import Client, InvalidResponseException
+from binance import Client, \
+    InvalidResponseException, StatusException
 
 client = Client('api_key', 'api_secret')
 
@@ -14,32 +15,21 @@ async def test_invalid_json():
             await client.get_products()
 
 
-# def test_api_exception():
-#     """Test API response Exception"""
+async def test_api_exception():
+    """Test Status Exception"""
+    with pytest.raises(StatusException):
+        with requests_mock.mock() as m:
+            json_obj = {"code": 1002, "msg": "Invalid API call"}
+            m.get('https://api.binance.com/api/v1/time', json=json_obj, status_code=400)
+            await client.get_server_time()
 
-#     with pytest.raises(BinanceAPIException):
-#         with requests_mock.mock() as m:
-#             json_obj = {"code": 1002, "msg": "Invalid API call"}
-#             m.get('https://api.binance.com/api/v1/time', json=json_obj, status_code=400)
-#             client.get_server_time()
+async def test_api_exception_invalid_json():
+    """
+    Test Status Exception, StatusException comes before InvalidResponseException
+    """
 
-
-# def test_api_exception_invalid_json():
-#     """Test API response Exception"""
-
-#     with pytest.raises(BinanceAPIException):
-#         with requests_mock.mock() as m:
-#             not_json_str = "<html><body>Error</body></html>"
-#             m.get('https://api.binance.com/api/v1/time', text=not_json_str, status_code=400)
-#             client.get_server_time()
-
-
-# def test_withdraw_api_exception():
-#     """Test Withdraw API response Exception"""
-
-#     with pytest.raises(BinanceWithdrawException):
-
-#         with requests_mock.mock() as m:
-#             json_obj = {"success": False, "msg": "Insufficient funds"}
-#             m.register_uri('POST', requests_mock.ANY, json=json_obj, status_code=200)
-#             client.withdraw(asset='BTC', address='BTCADDRESS', amount=100)
+    with pytest.raises(StatusException):
+        with requests_mock.mock() as m:
+            not_json_str = "<html><body>Error</body></html>"
+            m.get('https://api.binance.com/api/v1/time', text=not_json_str, status_code=400)
+            await client.get_server_time()
